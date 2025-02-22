e82a9cb5f28d83fb185219a0afde0904a45d666c
Didukh <didukhroma@gmail.com>
2025-02-22 06:58:10 +0000

    task 1 done

---
+++ task_1/hw01.py
@@ -0,0 +1,18 @@
+def caching_fibonacci():
+    
+    cash = {}
+    def fibonacci(n):        
+        if n <=0: return 0
+        if n == 1: return 1
+        if n in cash: return cash[n]
+
+        cash[n]=fibonacci(n-1)+ fibonacci(n-2)
+
+        return cash[n]        
+    
+    return fibonacci
+
+fib = caching_fibonacci()
+
+print(fib(10))
+print(fib(15))
